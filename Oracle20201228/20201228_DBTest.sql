fe8020a2a666a61251d63ccdbc2ce3ebe340cc98
mingJ7235 <joshuara7235@gmail.com>
2020-12-28 14:53:52 +0900

    #20201228 oracle first :100:

--- Oracle20201228/20201228_DBTest.sql
+++ Oracle20201228/20201228_DBTest.sql
@@ -94,4 +94,86 @@ SELECT * FROM emp WHERE deptno = 10 AND job ='과장';
 SELECT * FROM emp WHERE deptno = 10 or job ='과장';
 SELECT * FROM emp WHERE not deptno = 10 or job ='과장';
 
+commit;
+
+--급여가 400~500인경우
+SELECT * FROM emp WHERE sal >= 400 AND sal<= 500;
+--급여가 400미만이거나 500초과인경우
+SELECT * FROM emp WHERE sal <400 or sal >500;
+--between and 연산자 (급여가400~500) -> 컬렴명 between 숫자1 and 숫자2
+SELECT * FROM emp WHERE sal between 400 and 500;
+
+--in ~ : ~안에 
+--ex> 커미션이 80 이거나 100이거나 200인 사원
+SELECT * FROM emp WHERE comm=80 or comm=100 or comm=200;
+SELECT * FROM emp WHERE comm in(80,100,200); --in 사용
+
+--comm이 80이 아니고 100도 아니고 20도 아닌 사원
+SELECT * FROM emp WHERE comm != 80 and comm != 100 and comm != 200;
+SELECT * FROM emp WHERE comm not in (80,100,200);
+
+--유사일치 (비슷하게 일치하는것 검색 EX> 김씨 인 사람 찾기) ->like
+--와일드 카드 : % _
+--  % : 문자가 없거나 하나 이상의 어떤 문자가 와도 상관 없다. (여러글자를 대신)
+--  _ : 하나의 문자가 어떤 값이 와도 상관 없다. (한글자를 대신)
+SELECT * FROM emp WHERE ename like '조%';
+INSERT INTO EMP VALUES(1016, '조이성', '사원', 1006, to_date('2007-11-09','yyyy-mm-dd'), 250, NULL, 10);
+SELECT * FROM emp WHERE ename like '%이%';
+SELECT * FROM emp WHERE ename like '__성';
+
+SELECT * FROM emp WHERE job like '사%';
+--not사용해서 직위에서 사로 시작하지 않는사람
+SELECT * FROM emp WHERE job not like '사%';
+
+SELECT * FROM emp WHERE ename not like '이%';
+
+--comm이 null인 사람 찾기
+--SELCET * FROM emp WHERE comm = null; null은 못찾음
+--NULL을 찾기위해서는 IS NULL을 써야함
+SELECT * FROM emp WHERE comm is null;
+SELECT * FROM emp WHERE comm is not null;
+
+
+--정렬을 해서 java로 가지고 가는것이 프로그램적으로 편하다 
+--order by 를 사용하여 정렬한다
+
+SELECT * FROM emp ORDER BY sal; --기본 정렬은 오름차순으로되어있다
+SELECT * FROM emp ORDER BY sal DESC; --DESCENDING (내림차순)
+SELECT * FROM emp ORDER BY sal DESC, deptno DESC; --어떤걸 먼저 쓰느냐에 따라 정렬이 달라짐 
+SELECT * FROM emp ORDER BY deptno DESC, sal DESC;
+
+SELECT * FROM emp ORDER BY ename DESC;
+
+--사원중에서 급여를 오름차순으로 정렬
+SELECT * FROM emp WHERE job ='사원' ORDER BY sal;
+
+--최근에 입사한 사람 순서대로
+SELECT * FROM emp WHERE NOT job ='사장' ORDER BY hiredate DESC;
+
+--사원 이름과 급여일 입사일자만 출력하기
+SELECT ename, sal, hiredate FROM emp;
+--부서번호는 dno로 부서명은 dname으로 정해져 별층을 붙여서 출력
+SELECT deptno dno, dname 부서명 FROM dept;
+--사원테이블에 존재하는 직급의 종류만 출력
+SELECT job FROM emp;
+--급여가 300 이하인 사원의 사원번호, 사원이름, 급여 출력
+SELECT empno 사원번호, ename 이름, sal 급혀 FROM emp WHERE job = '사원' and sal <= 300;
+--이름이 '오지호'인 사원의 사원번호, 사원명, 급여를 출력
+SELECT empno, ename, sal FROM emp WHERE ename ='오지호';
+--급여가 250 이거나 300이거나 500인 사원들의 사원번호, 사원명, 급여
+SELECT empno, ename, sal FROM emp WHERE sal in (250, 300, 500);
+-- 사원중에는 이름이 '김'으로 시작하는 사람과 이름중에 '기'를 포함하는 사원의 사원번호와 사원이름을 출력하세요
+SELECT empno, ename FROM emp WHERE ename like '김%' or ename like '%기%' ;
+-- 상급자가 없는 사원을 검색하세요 
+SELECT * FROM emp WHERE mgr is null;
+-- 사원테이블의 자료를 입사일을 기준으로 정렬하여 최근에 입사한 직원을 가장 먼저 출력하세요
+--  (단, 사원번호, 사원명, 직급, 입사일) 컬럼만 출력
+SELECT empno, ename, job, hiredate FROM emp ORDER BY hiredate desc;
+-- 부서번호가 빠른 사원번호부터 출력하되 같은 부서 내의 사원을 출력할 경우 입사한지 가장 오래된 사원부터 출력하세요
+SELECT * FROM emp ORDER BY deptno, hiredate;
+
+
+
+
+
 
