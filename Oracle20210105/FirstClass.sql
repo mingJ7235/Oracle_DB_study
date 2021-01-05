e553a996b9d55a952cccd7acfd2f0f318e0709ab
mingJ7235 <joshuara7235@gmail.com>
2021-01-05 10:20:19 +0900

    20210105 Oracle study (rollback, transaction concept)

---
+++ Oracle20210105/FirstClass.sql
@@ -0,0 +1,71 @@
+--insert study ppt 118쪽
+
+--DML : 데이터 조작 언어 
+
+create table dept01 (
+    deptno number(4),
+    dname varchar2(14),
+    loc varchar2(13));
+
+--insert 전체형식으로 다 입력하기
+
+insert into dept01 (deptno, dname, loc) values (10, '경리부', '서울');
+insert into dept01 (loc, dname, deptno) values ('인천', '전산부', 20); --컬럼 순서가 달라도 기입 가능
+
+--생략하기(구조순서대로)
+insert into dept01 values (30, '영업부', '부산');
+insert into dept01 values (30, '영업부', 10);
+insert into dept01 values ('30', '영업부', 10);
+
+select * from emp02;
+
+--null에 대하여 입력하기
+
+insert into dept01 values (40, '인사부', null);
+insert into dept01 values (50, '인사부', ''); --''를하는것도 null이되는것임
+insert into dept01 (deptno, dname) values (20, '경리부);
+
+drop table emp02;
+
+create table emp02 (
+    empno number(4),
+    ename varchar2(10),
+    job varchar2(9),
+    hiredate date,
+    deptno number(2));
+    
+--테이블의 구조보기
+desc emp02;
+
+INSERT INTO emp02 VALUES ( 1001, '김사랑','사원','05/1월/21', 20);
+insert into emp02 values ( 1002, '한예슬', '대리', to_date('2021/01/10','yyyy/mm/dd'), 10);
+
+--시스템에서 오늘 날짜로 날짜의 자료를 넣기
+insert into emp02 values (1003, '최경희', '사원', sysdate, 30);
+
+select * from dept01;
+--데이터 수정하기 update
+update dept01
+    set dname='생산부'
+    where deptno = 10;
+
+commit;
+
+update dept01
+    set dname='총무부', loc ='서울'
+    where deptno = 50;
+
+update dept01
+    set loc = '서울';
+    
+update dept01
+    set deptno = deptno + 100;
+
+--commit한 상태로 돌아감
+rollback;
+
+--데이터 삭제
+
+
+
+
