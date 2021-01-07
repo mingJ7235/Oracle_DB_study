9c4701feb8a181c2aacb196f828226a0286b241f
mingJ7235 <joshuara7235@gmail.com>
2021-01-07 10:57:57 +0900

    Second class for outer join :herb:

--- Oracle20210107/02-2 second class outer join practice.sql
+++ Oracle20210107/02-2 second class outer join practice.sql
@@ -11,3 +11,36 @@
 
 --1) left join
 
+--Student 테이블과 professor 테이블을 조인하여 학생이름과 지도교수이름을 출력하시오
+--단, 지도교수가 결정되지 않은 학생의명단도 출력하시오 -> outer join을 해야함
+
+--oracle outer join 문법
+select s.name "학생" , p.name "교수" from student s, professor p
+    where s.profno = p.profno(+);
+    
+--ANSI outer join 문법
+
+--1) left기준 즉, 학생 기준으로 조회 (학생은 전체 조회가된다.)
+select s.name "학생", p.name "교수" from student s
+    left outer join professor p
+    on s.profno= p.profno;
+    
+--2) right기준 즉, 교수기준으로 조회 (교수가 전체 조회가된다.) 즉, 지도학생이 결정안된 교수명단 출력
+select s.name "학생", p.name "교수" from student s
+    right outer join professor p
+    on s.profno= p.profno;
+
+--3) 지도학생이 결정안된 교수명단, 지도교수가 결정안된 학생명단 한번에 모두 -> outer join
+select s.name "학생" , p.name "교수" 
+    from student s full outer join professor p
+    on s.profno = p.profno;
+--3)번을 oracle outer join 문법으로해보기
+select s.name "학생", p.name "교수"
+    from student s, professor p
+    where s.profno(+) = p.profno
+    union
+    select s.name "학생", p.name "교수"
+    from student s, professor p
+    where s.profno = p.profno(+);
+    
+    
